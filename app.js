29b7339c91e75f4e36aff1510f77e90f75229a11
Uptimum <uptimumdn2000@gmail.com>
2021-11-12 19:33:42 +0700

    .

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const axios = require('axios');
-const shortid = require('shortid');
 const list = [];
 
 app.use(express.urlencoded({ extended: false }));
